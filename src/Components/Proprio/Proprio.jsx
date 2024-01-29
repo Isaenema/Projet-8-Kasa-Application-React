cc11aa89683845fd440b1f34a6209f78b379e48c
Pierre Comte <p.s.comte@hotmail.fr>
2024-01-29 12:57:05 +0100

    bug commit

---
+++ src/Components/Proprio/Proprio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Proprio.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+function Proprio() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faChevronUp} />
+    </div>
+  );
+}
+
+export default Proprio;
